2e49fb3a1af108b18fed453faef5478be0fa36a2
Kenaniah M Subrahmanyam <89816670+ken3512@users.noreply.github.com>
2022-10-27 01:49:17 -0400

    Added images for projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -8,18 +8,28 @@
                 <h1 class="project_title"><span class="badge">Dimension - Game Engine & Game</span></h1>
                 <p class="timeline-subtitle">March 2022 - December 2022, Electron.js, Node.js, and P5.js</p>
                 <h4 class="project_info"></h4>
+                <img class="pfp" src="../assets/ClubEvent1.png"/><br>
+                <img class="pfp" src="../assets/ClubEvent2.png"/><br>
+                <img class="pfp" src="../assets/ClubEvent3.png"/><br>
+                <img class="pfp" src="../assets/ClubEvent4.png"/><br>
             </section>
 
             <section>
                 <h1 class="project_title"><span class="badge">Club Event Organizer  - Social Media</span></h1>
                 <p class="timeline-subtitle">March 2022 – April 2022, HTML & CSS, PHP, JavaScript, and MySQL</p>
                 <h4 class="project_info"></h4>
+                <img class="pfp" src="../assets/Dimension1.png"/><br>
+                <img class="pfp" src="../assets/Dimension2.png"/><br>
             </section>
 
             <section>
                 <h1 class="project_title"><span class="badge">Dungoen Run - Web Game</span></h1>
                 <p class="timeline-subtitle">November 2021 – December 2021, MongoDB, Express.js, React, Node.js, and P5.js</p>
                 <h4 class="project_info"></h4>
+                <img class="pfp" src="../assets/DungeonRun1.png"/><br>
+                <img class="pfp" src="../assets/DungeonRun2.png"/><br>
+                <img class="pfp" src="../assets/DungeonRun3.png"/><br>
+                <img class="pfp" src="../assets/DungeonRun4.png"/><br>
             </section>
 
             <section>
@@ -76,5 +86,9 @@ $secondary: #4f537b;
         margin: auto;
         width: min(800px, 85%);
         padding: 15px;
+    }
+
+    .pfp{
+        width: 200px;
     }
 </style>
