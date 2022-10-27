38e17f1f82bc9ba742b5fc8e636b570c39d343c4
Kenaniah M Subrahmanyam <89816670+ken3512@users.noreply.github.com>
2022-10-27 00:38:13 -0400

    Added images

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -7,31 +7,31 @@
             <section>
                 <h1 class="project_title"><span class="badge">Dimension - Game Engine & Game</span></h1>
                 <p class="timeline-subtitle">March 2022 - December 2022, Electron.js, Node.js, and P5.js</p>
-                <h4 class="project_info">Hello my name is Kenaniah Subrahmanyam. I first started coding at about 13 and I am currently a senior at UCF as a Computer Science major. I have managed to keep a 3.97 GPA throughout my degree while working part time. I have learned many skills along the way!</h4>
+                <h4 class="project_info"></h4>
             </section>
 
             <section>
                 <h1 class="project_title"><span class="badge">Club Event Organizer  - Social Media</span></h1>
                 <p class="timeline-subtitle">March 2022 – April 2022, HTML & CSS, PHP, JavaScript, and MySQL</p>
-                <h4 class="project_info">Hello my name is Kenaniah Subrahmanyam. I first started coding at about 13 and I am currently a senior at UCF as a Computer Science major. I have managed to keep a 3.97 GPA throughout my degree while working part time. I have learned many skills along the way!</h4>
+                <h4 class="project_info"></h4>
             </section>
 
             <section>
                 <h1 class="project_title"><span class="badge">Dungoen Run - Web Game</span></h1>
                 <p class="timeline-subtitle">November 2021 – December 2021, MongoDB, Express.js, React, Node.js, and P5.js</p>
-                <h4 class="project_info">Hello my name is Kenaniah Subrahmanyam. I first started coding at about 13 and I am currently a senior at UCF as a Computer Science major. I have managed to keep a 3.97 GPA throughout my degree while working part time. I have learned many skills along the way!</h4>
+                <h4 class="project_info"></h4>
             </section>
 
             <section>
                 <h1 class="project_title"><span class="badge">Compiler</span></h1>
                 <p class="timeline-subtitle">June 2021 – July 2021, C</p>
-                <h4 class="project_info">Hello my name is Kenaniah Subrahmanyam. I first started coding at about 13 and I am currently a senior at UCF as a Computer Science major. I have managed to keep a 3.97 GPA throughout my degree while working part time. I have learned many skills along the way!</h4>
+                <h4 class="project_info"></h4>
             </section>
 
             <section>
                 <h1 class="project_title"><span class="badge">Command line</span></h1>
                 <p class="timeline-subtitle">November 2021 – December 2021, C</p>
-                <h4 class="project_info">Hello my name is Kenaniah Subrahmanyam. I first started coding at about 13 and I am currently a senior at UCF as a Computer Science major. I have managed to keep a 3.97 GPA throughout my degree while working part time. I have learned many skills along the way!</h4>
+                <h4 class="project_info"></h4>
             </section>
         </div>
         <br>
